0868f0ab76b2ea33401632ef93c9fc0b22232506
kiwowki <wolves941110@gmail.com>
2024-01-09 05:03:15 +0900

    2024.01.09

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,25 +6,25 @@ const Header = () => {
         <header id="header" role="banner">
             <nav className="left" role="navigation">
                 <div className="visual">visual</div>
-                <div className="about">
-                    <Link to="#">about</Link>
+                {/* <div className="about">
+                    <Link to="/">about</Link>
                 </div>
                 <div className="works">
-                    <Link to="#">works</Link>
+                    <Link to="/">works</Link>
                 </div>
                 <div className="skills">
-                    <Link to="#">skills</Link>
+                    <Link to="/">skills</Link>
                 </div>
                 <div className="playground">
-                    <Link to="#">playground</Link>
+                    <Link to="/">playground</Link>
                 </div>
                 <div className="contact">
-                    <Link to="#">contact</Link>
-                </div>
+                    <Link to="/">contact</Link>
+                </div> */}
             </nav>
             <nav className="right" role="navigation">
                 <div className="comment">
-                    <Link to="#">comment</Link>
+                    <Link to="/">comment</Link>
                 </div>
             </nav>
         </header>
