54d1a5d0c309bb1ae794489541b75b0f0e200293
kiwowki <wolves941110@gmail.com>
2024-01-12 12:08:55 +0900

    24.01.12배포테스트

---
+++ App/server/config/dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI : "mongodb+srv://wolves941110:rkdtjr00!!@cluster0.efj98qh.mongodb.net/?retryWrites=true&w=majority",
+    access_key: "EcrjPAi1beyldItf2b9n",
+    secret_key: "6Od2GbH8tEysHMKKyT9q9AJ7le5SUUJ7Mzzmo3fH"
+}
